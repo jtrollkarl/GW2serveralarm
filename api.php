9b82165273a07899cd74b7059b4cd075f4d55c1b
Alexander Vestling <TurquoiseWater@users.noreply.github.com>
2017-11-05 19:20:05 +0100

    Add files via upload

---
+++ api.php
@@ -0,0 +1,43 @@
+<?php
+
+if(isset($_GET['send_notification'])){
+   send_notification ();
+}
+
+function send_notification()
+{
+	echo 'Hello';
+define( 'API_ACCESS_KEY', 'YOUR API KEY HERE');
+ //   $registrationIds = ;
+#prep the bundle
+     $msg = array
+          (
+		'body' 	=> 'Alex Firebase Push Notification',
+		'title'	=> 'Alex test',
+             	
+          );
+	$fields = array
+			(
+				'to'		=> $_REQUEST['token'],
+				'notification'	=> $msg
+			);
+	
+	
+	$headers = array
+			(
+				'Authorization: key=' . API_ACCESS_KEY,
+				'Content-Type: application/json'
+			);
+#Send Reponse To FireBase Server	
+		$ch = curl_init();
+		curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+		curl_setopt( $ch,CURLOPT_POST, true );
+		curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+		curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+		curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+		$result = curl_exec($ch );
+		echo $result;
+		curl_close( $ch );
+}
+?>
