52ba8222025e0c647afd512d0f3bb9036b4a4170
Justin Inácio <j.trollkarl@gmail.com>
2018-01-18 16:33:50 +0100

    build, update
    - added Timber library
    - added an init to application class if app is a debug build

--- app/src/main/java/com/moducode/gw2serveralarm/Gw2Application.java
+++ app/src/main/java/com/moducode/gw2serveralarm/Gw2Application.java
@@ -1,8 +1,11 @@
 package com.moducode.gw2serveralarm;
 
 import android.app.Application;
+import android.os.Build;
 
 import com.squareup.leakcanary.LeakCanary;
+
+import timber.log.Timber;
 
 /**
  * Created by Jay on 2017-11-10.
@@ -13,10 +16,17 @@ public class Gw2Application extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initTimber();
         if (LeakCanary.isInAnalyzerProcess(this)) {
             return;
         }
         LeakCanary.install(this);
+        
     }
 
+    private void initTimber(){
+        if(BuildConfig.DEBUG){
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
 }
